46813478e842b4e937c37d5a517544d3319548c5
Rea Redlich <reare@etoro.com>
2022-05-18 22:55:21 +0300

    More code cleaning continue

--- Tests/demoBlazeTests.py
+++ Tests/demoBlazeTests.py
@@ -18,34 +18,34 @@ def test_validate_for_nexus6_product():
 
 
 def get_user_cookie_via_login():
-    user_login_details = {"username": USERNAME, "password": get_password_base64().decode()}
-    login_response = requests.post(f"{DEMO_BLAZE_BASE_URL}/login", json=user_login_details)
-    assert login_response.status_code == EXPECTED_STATUS_CODE_OK
-
+    login_response = login()
     return get_user_cookie(login_response)
 
 
 def verify_items_count_and_get_product_id(user_cookie):
-    user_credentials_data = {"cookie": user_cookie, "flag": True}
-    view_cart_response = requests.post(f"{DEMO_BLAZE_BASE_URL}/viewcart", json=user_credentials_data)
-    assert view_cart_response.status_code == EXPECTED_STATUS_CODE_OK
+    view_cart_response = get_list_of_items(user_cookie)
 
     view_cart_response_json_body = view_cart_response.json()
     verify_items_count(view_cart_response_json_body, EXPECTED_ITEMS_COUNT)
     verify_product_id_after_view_cart_api(view_cart_response_json_body, INDEX, EXPECTED_PRODUCT_NEXUS6_ID)
 
-    return view_cart_response_json_body["Items"][INDEX]["prod_id"]
+    return get_prod_id(view_cart_response_json_body)
 
 
 def verify_product(product_id):
-    product_data = {"id": product_id}
-    view_response = requests.post(f"{DEMO_BLAZE_BASE_URL}/view", json=product_data)
-    assert view_response.status_code == EXPECTED_STATUS_CODE_OK
+    view_response = get_product_data(product_id)
 
     view_response_json_body = view_response.json()
     verify_product_id_after_view_api(view_response_json_body, product_id)
     verify_product_title(view_response_json_body, EXPECTED_PRODUCT_TITLE)
     verify_product_price(view_response_json_body, EXPECTED_PRODUCT_NEXUS6_PRICE)
+
+
+def login():
+    user_login_details = {"username": USERNAME, "password": get_password_base64().decode()}
+    login_response = requests.post(f"{DEMO_BLAZE_BASE_URL}/login", json=user_login_details)
+    assert login_response.status_code == EXPECTED_STATUS_CODE_OK
+    return login_response
 
 
 def get_password_base64():
@@ -55,6 +55,24 @@ def get_password_base64():
 
 def get_user_cookie(login_response):
     return login_response.content.decode().replace('\"\n', "").split(": ")[1]
+
+
+def get_list_of_items(user_cookie):
+    user_credentials_data = {"cookie": user_cookie, "flag": True}
+    view_cart_response = requests.post(f"{DEMO_BLAZE_BASE_URL}/viewcart", json=user_credentials_data)
+    assert view_cart_response.status_code == EXPECTED_STATUS_CODE_OK
+    return view_cart_response
+
+
+def get_prod_id(view_cart_response_json_body):
+    return view_cart_response_json_body["Items"][INDEX]["prod_id"]
+
+
+def get_product_data(product_id):
+    product_data = {"id": product_id}
+    view_response = requests.post(f"{DEMO_BLAZE_BASE_URL}/view", json=product_data)
+    assert view_response.status_code == EXPECTED_STATUS_CODE_OK
+    return view_response
 
 
 def verify_items_count(view_cart_response_json_body, expected_count):
